72ec9770dffa8b2dd11ba191b61773725d19b260
Joseph Nolan <josephbnolan@me.com>
2016-02-26 09:35:22 -0600

    34% done

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -1,16 +1,11 @@
-(function() {
+angular
+  .module('udemyAngular')
+    .controller('classifiedsCtrl', function($scope) {
 
-  "use strict";
-  angular
-    .module('udemyAngular')
-      .controller('classifiedsCtrl', function($scope) {
+      $scope.classified = {
+        title: "First Item",
+        price: "$1,000,000",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      }
 
-        $scope.name = {
-          first: 'Joseph',
-          last: 'Nolan'
-        };
-
-        $scope.message = 'hello, world'
-
-      });
-})();
+});
